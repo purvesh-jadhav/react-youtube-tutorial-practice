082e7ff19b87ff30c1f03be834e30ac8252ad47c
Purvesh Jadhav <purveshjadhav05@gmail.com>
2026-06-23 01:13:23 +0530

    learn about external style in reactjs

---
+++ external-style-in-reactjs/src/App.jsx
@@ -0,0 +1,147 @@
+import React from "react";
+import "./CSS/style.css";
+
+const App = () => {
+  return (
+    <div>
+      <h2>External style in react.js</h2>
+      <div className="card-container">
+        <div className="card">
+          <div className="img-section">
+            <img
+              src="https://imgs.search.brave.com/dRfz574cyJjb2tmXgMoOkPALpC6ytLyLQbLaOVDROXc/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pLnBp/bmltZy5jb20vb3Jp/Z2luYWxzL2M2L2E0/L2VhL2M2YTRlYTgy/YTQ4ZDdkNGU5OTc4/OTRkMjQ1YTZiZjQ5/LmpwZw"
+              alt=""
+            />
+          </div>
+          <div className="details-section">
+            <h3 id="name">Name: John Smith</h3>
+            <h4 id="role">Role: Software Developer</h4>
+            <h4 id="age">Age: 29</h4>
+          </div>
+        </div>
+
+        <div className="card">
+          <div className="img-section">
+            <img
+              src="https://imgs.search.brave.com/dRfz574cyJjb2tmXgMoOkPALpC6ytLyLQbLaOVDROXc/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pLnBp/bmltZy5jb20vb3Jp/Z2luYWxzL2M2L2E0/L2VhL2M2YTRlYTgy/YTQ4ZDdkNGU5OTc4/OTRkMjQ1YTZiZjQ5/LmpwZw"
+              alt=""
+            />
+          </div>
+          <div className="details-section">
+            <h3 id="name">Name: John Smith</h3>
+            <h4 id="role">Role: Software Developer</h4>
+            <h4 id="age">Age: 29</h4>
+          </div>
+        </div>
+
+        <div className="card">
+          <div className="img-section">
+            <img
+              src="https://imgs.search.brave.com/dRfz574cyJjb2tmXgMoOkPALpC6ytLyLQbLaOVDROXc/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pLnBp/bmltZy5jb20vb3Jp/Z2luYWxzL2M2L2E0/L2VhL2M2YTRlYTgy/YTQ4ZDdkNGU5OTc4/OTRkMjQ1YTZiZjQ5/LmpwZw"
+              alt=""
+            />
+          </div>
+          <div className="details-section">
+            <h3 id="name">Name: John Smith</h3>
+            <h4 id="role">Role: Software Developer</h4>
+            <h4 id="age">Age: 29</h4>
+          </div>
+        </div>
+
+        
+        <div className="card">
+          <div className="img-section">
+            <img
+              src="https://imgs.search.brave.com/dRfz574cyJjb2tmXgMoOkPALpC6ytLyLQbLaOVDROXc/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pLnBp/bmltZy5jb20vb3Jp/Z2luYWxzL2M2L2E0/L2VhL2M2YTRlYTgy/YTQ4ZDdkNGU5OTc4/OTRkMjQ1YTZiZjQ5/LmpwZw"
+              alt=""
+            />
+          </div>
+          <div className="details-section">
+            <h3 id="name">Name: John Smith</h3>
+            <h4 id="role">Role: Software Developer</h4>
+            <h4 id="age">Age: 29</h4>
+          </div>
+        </div>
+
+        
+        <div className="card">
+          <div className="img-section">
+            <img
+              src="https://imgs.search.brave.com/dRfz574cyJjb2tmXgMoOkPALpC6ytLyLQbLaOVDROXc/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pLnBp/bmltZy5jb20vb3Jp/Z2luYWxzL2M2L2E0/L2VhL2M2YTRlYTgy/YTQ4ZDdkNGU5OTc4/OTRkMjQ1YTZiZjQ5/LmpwZw"
+              alt=""
+            />
+          </div>
+          <div className="details-section">
+            <h3 id="name">Name: John Smith</h3>
+            <h4 id="role">Role: Software Developer</h4>
+            <h4 id="age">Age: 29</h4>
+          </div>
+        </div>
+
+        
+        <div className="card">
+          <div className="img-section">
+            <img
+              src="https://imgs.search.brave.com/dRfz574cyJjb2tmXgMoOkPALpC6ytLyLQbLaOVDROXc/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pLnBp/bmltZy5jb20vb3Jp/Z2luYWxzL2M2L2E0/L2VhL2M2YTRlYTgy/YTQ4ZDdkNGU5OTc4/OTRkMjQ1YTZiZjQ5/LmpwZw"
+              alt=""
+            />
+          </div>
+          <div className="details-section">
+            <h3 id="name">Name: John Smith</h3>
+            <h4 id="role">Role: Software Developer</h4>
+            <h4 id="age">Age: 29</h4>
+          </div>
+        </div>
+
+        
+        <div className="card">
+          <div className="img-section">
+            <img
+              src="https://imgs.search.brave.com/dRfz574cyJjb2tmXgMoOkPALpC6ytLyLQbLaOVDROXc/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pLnBp/bmltZy5jb20vb3Jp/Z2luYWxzL2M2L2E0/L2VhL2M2YTRlYTgy/YTQ4ZDdkNGU5OTc4/OTRkMjQ1YTZiZjQ5/LmpwZw"
+              alt=""
+            />
+          </div>
+          <div className="details-section">
+            <h3 id="name">Name: John Smith</h3>
+            <h4 id="role">Role: Software Developer</h4>
+            <h4 id="age">Age: 29</h4>
+          </div>
+        </div>
+
+        
+        <div className="card">
+          <div className="img-section">
+            <img
+              src="https://imgs.search.brave.com/dRfz574cyJjb2tmXgMoOkPALpC6ytLyLQbLaOVDROXc/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pLnBp/bmltZy5jb20vb3Jp/Z2luYWxzL2M2L2E0/L2VhL2M2YTRlYTgy/YTQ4ZDdkNGU5OTc4/OTRkMjQ1YTZiZjQ5/LmpwZw"
+              alt=""
+            />
+          </div>
+          <div className="details-section">
+            <h3 id="name">Name: John Smith</h3>
+            <h4 id="role">Role: Software Developer</h4>
+            <h4 id="age">Age: 29</h4>
+          </div>
+        </div>
+
+        
+        <div className="card">
+          <div className="img-section">
+            <img
+              src="https://imgs.search.brave.com/dRfz574cyJjb2tmXgMoOkPALpC6ytLyLQbLaOVDROXc/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pLnBp/bmltZy5jb20vb3Jp/Z2luYWxzL2M2L2E0/L2VhL2M2YTRlYTgy/YTQ4ZDdkNGU5OTc4/OTRkMjQ1YTZiZjQ5/LmpwZw"
+              alt=""
+            />
+          </div>
+          <div className="details-section">
+            <h3 id="name">Name: John Smith</h3>
+            <h4 id="role">Role: Software Developer</h4>
+            <h4 id="age">Age: 29</h4>
+          </div>
+        </div>
+
+        
+      </div>
+    </div>
+  );
+};
+
+export default App;
