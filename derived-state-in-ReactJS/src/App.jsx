e0a33d8dfc3ff2568f38d437776dcdd1aaeff841
Purvesh Jadhav <purveshjadhav05@gmail.com>
2026-07-06 23:44:47 +0530

    learn about derived state in React JS

---
+++ derived-state-in-ReactJS/src/App.jsx
@@ -0,0 +1,33 @@
+  import React, { useState } from "react";
+
+  const App = () => {
+    const [users, setUsers] = useState([]);
+    const [user, setUser] = useState("");
+
+    function handleAllUsers() {
+      setUsers([...users, user]);
+    }
+
+    const total=users.length;
+    const last=users[users.length-1]
+
+    return (
+      <div>
+        <h2>Total User : {total}</h2>
+        <h2>Last User : {last}</h2>
+        <input
+          onChange={(event) => setUser(event.target.value)}
+          type="text"
+          placeholder="Add new user"
+        />
+
+        <button onClick={handleAllUsers}>Add User</button>
+
+        {users.map((item, index) => (
+          <h4 key={index}>{item}</h4>
+        ))}
+      </div>
+    ); 
+  };
+
+  export default App;
